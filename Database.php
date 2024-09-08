e7978e0a5d59bd29385461e9aa2746d81faeac3b
Elad Moshe <pheromem@gmail.com>
2024-09-08 17:52:52 +0300

    Added a select method to the Database class

--- Database.php
+++ Database.php
@@ -32,4 +32,21 @@ class Database
             echo "Failed to connect to database: " . $e->getMessage();
         }
     }
+
+    public function select(string $table, array $columns = ["*"], array $conditions = [])
+    {
+        if (isset($conn)) {
+            $strColumns = implode(", ", $columns);
+            $query = "SELECT $strColumns FROM $table";
+            if (!empty($conditions)) {
+                $strConditions = implode(" AND ", $conditions);
+                $query .= " WHERE $strConditions";
+            }
+            $stmt = $conn->prepare($query);
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } else {
+            echo "Please connect to database first.";
+        }
+    }
 }
