d6c9801a22b80c3bc354413d9c37eeffa2ab417d
Elad Moshe <pheromem@gmail.com>
2024-09-10 14:37:52 +0300

    Added some simple styling

--- PostView.php
+++ PostView.php
@@ -26,38 +26,44 @@ class PostView {
         ?>
             <article class="post">
                 <div class=user-data>
-                        <img class="user-avatar" src=<?php echo AVATAR_FILE_NAME ?> alt="Avatar">
+                        <div class="user-avatar-container">
+                            <img class="user-avatar" src=<?php echo AVATAR_FILE_NAME ?> alt="Avatar">
+                        </div>
                         <h2 class="user-name"> <?php echo $this->userName ?> </h2>
                         <p class="user-email"> <?php echo $this->userEmail ?> </p>
                         <?php
                             if ($this->userActive) {
                                 ?>
-                                <p class="is_user_active">Active</p>
+                                <p class="is-user-active">Active</p>
                                 <?php
                             }
                             else {
                                 ?>
-                                <p class="is_user_active">Inactive</p>
+                                <p class="is-user-active">Inactive</p>
                                 <?php
                             }
                         ?>
                     </div>
                     <div class=post-data>
-                        <h1 class="post-title"> <?php echo $this->postTitle ?> </h1>
-                        <p class="post-body"> <?php echo $this->postBody ?> </p>
-                        <p class="post-date-created"> <?php echo $this->postDateCreated ?> </p>
-                        <?php
-                            if ($this->postActive) {
-                                ?>
-                                <p class="is_post_active">Active</p>
-                                <?php
-                            }
-                            else {
-                                ?>
-                                <p class="is_post_active">Inactive</p>
-                                <?php
-                            }
-                        ?>
+                        <div class="post-content">
+                            <h1 class="post-title"> <?php echo $this->postTitle ?> </h1>
+                            <p class="post-body"> <?php echo $this->postBody ?> </p>
+                        </div>
+                        <div class="post-metadata">
+                            <p class="post-date-created"> <?php echo $this->postDateCreated ?> </p>
+                            <?php
+                                if ($this->postActive) {
+                                    ?>
+                                    <p class="is-post-active">Active</p>
+                                    <?php
+                                }
+                                else {
+                                    ?>
+                                    <p class="is-post-active">Inactive</p>
+                                    <?php
+                                }
+                            ?>
+                        </div>
                     </div>
             </article>
         <?php
