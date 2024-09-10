b236190270ab525e800bbeb26893aea3df10c662
Elad Moshe <pheromem@gmail.com>
2024-09-10 13:47:40 +0300

    Added article tag to PostView

--- PostView.php
+++ PostView.php
@@ -24,40 +24,42 @@ class PostView {
 
     public function display() {
         ?>
-            <div class=user-data>
-                <img class="user-avatar" src=<?php echo AVATAR_FILE_NAME ?> alt="Avatar">
-                <h2 class="user-name"> <?php echo $this->userName ?> </h2>
-                <p class="user-email"> <?php echo $this->userEmail ?> </p>
-                <?php
-                    if ($this->userActive) {
-                        ?>
-                        <p class="is_user_active">Active</p>
+            <article class="post">
+                <div class=user-data>
+                        <img class="user-avatar" src=<?php echo AVATAR_FILE_NAME ?> alt="Avatar">
+                        <h2 class="user-name"> <?php echo $this->userName ?> </h2>
+                        <p class="user-email"> <?php echo $this->userEmail ?> </p>
                         <?php
-                    }
-                    else {
+                            if ($this->userActive) {
+                                ?>
+                                <p class="is_user_active">Active</p>
+                                <?php
+                            }
+                            else {
+                                ?>
+                                <p class="is_user_active">Inactive</p>
+                                <?php
+                            }
                         ?>
-                        <p class="is_user_active">Inactive</p>
+                    </div>
+                    <div class=post-data>
+                        <h1 class="post-title"> <?php echo $this->postTitle ?> </h1>
+                        <p class="post-body"> <?php echo $this->postBody ?> </p>
+                        <p class="post-date-created"> <?php echo $this->postDateCreated ?> </p>
                         <?php
-                    }
-                ?>
-            </div>
-            <div class=post-data>
-                <h1 class="post-title"> <?php echo $this->postTitle ?> </h1>
-                <p class="post-body"> <?php echo $this->postBody ?> </p>
-                <p class="post-date-created"> <?php echo $this->postDateCreated ?> </p>
-                <?php
-                    if ($this->postActive) {
+                            if ($this->postActive) {
+                                ?>
+                                <p class="is_post_active">Active</p>
+                                <?php
+                            }
+                            else {
+                                ?>
+                                <p class="is_post_active">Inactive</p>
+                                <?php
+                            }
                         ?>
-                        <p class="is_post_active">Active</p>
-                        <?php
-                    }
-                    else {
-                        ?>
-                        <p class="is_post_active">Inactive</p>
-                        <?php
-                    }
-                ?>
-            </div>
+                    </div>
+            </article>
         <?php
     }
 
